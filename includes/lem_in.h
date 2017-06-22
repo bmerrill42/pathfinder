5a7fd07fc7a07c2399e5e3ba3187efb12751d033
Thomas Pan <tpan@e1z2r5p8.42.us.org>
2017-06-21 18:08:27 -0700

    added error codes

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/03 20:18:06 by tpan              #+#    #+#             */
-/*   Updated: 2017/06/03 20:19:28 by tpan             ###   ########.fr       */
+/*   Updated: 2017/06/21 17:36:25 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,19 @@
 # define P2			"    +:+ +:+         +:+  \n  +#+  +:+       +#+     \n" P3
 # define P3			"+#+#+#+#+#+   +#+        \n     #+#    #+#          \n" P4
 # define P4			"    ###   ########.us.org    \n"
+
+typedef enum		e_error
+{
+	GENERIC,
+	NO_ANTS,
+	NO_END,
+	DUP_END,
+	NO_START,
+	DUP_START,
+	NO_ROOMS,
+	NO_VALID_PATH,
+	DUP_NAME,
+	MALLOC_FAIL
+}					t_error;
 
 #endif
