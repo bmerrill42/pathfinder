42f4b4158df1562dd0fa4a956f14692c6e80103c
Thomas Pan <tpan@e1z2r5p8.42.us.org>
2017-06-27 00:21:32 -0700

    norme

--- srcs/init.c
+++ srcs/init.c
@@ -1,6 +1,6 @@
 #include "../includes/lem_in.h"
 
-void	init_rooms(t_room **rooms,t_room **tmp, int i)
+void	init_rooms(t_room **rooms, t_room **tmp, int i)
 {
 	(*tmp)[i].name = ft_strdup((*rooms)[i].name);
 	(*tmp)[i].start = (*rooms)[i].start;
@@ -8,4 +8,18 @@ void	init_rooms(t_room **rooms,t_room **tmp, int i)
 	(*tmp)[i].visited = (*rooms)[i].visited;
 	(*tmp)[i].connections = (*rooms)[i].connections;
 	(*tmp)[i].c_count = (*rooms)[i].c_count;
+}
+
+void	set_start_room(t_lemin *everything)
+{
+	everything->start = everything->r_ct - 1;
+	everything->rooms[everything->r_ct - 1].start = TRUE;
+	return ;
+}
+
+void	set_end_room(t_lemin *everything)
+{
+	everything->end = everything->r_ct - 1;
+	everything->rooms[everything->r_ct - 1].end = TRUE;
+	return ;
 }
