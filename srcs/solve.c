4c6d50241a9d6656d6753d7b94eb91d5bedc0ed4
Brett Merrill <bmerrill@e1z2r5p10.42.us.org>
2017-06-26 19:10:21 -0700

    doesn't do any bad frees

--- srcs/solve.c
+++ srcs/solve.c
@@ -6,7 +6,7 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/24 20:14:08 by tpan              #+#    #+#             */
-/*   Updated: 2017/06/26 18:38:54 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/06/26 19:09:56 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,18 @@ void		add_path(t_path *path, int index)
 
 void		remove_path(t_path *path)
 {
-	path->pathfinder[path->index] = -1;
+	int *tmp;
+	int i;
+
+	i = 0;
+	tmp = (int*)malloc(((*path).index + 1) * sizeof(int));
+	while (i < (*path).index)
+	{
+	    tmp[i] = (*path).pathfinder[i];
+	    i++;
+	}
+	free((*path).pathfinder);
+	(*path).pathfinder = tmp;
 	path->index--;
 }
 
@@ -81,57 +92,3 @@ int		find_path(t_room **rooms, t_path *path, int index)
 	}
 	return (1);
 }
-
-/*int	main(int argc, char **argv)
-{
-	t_lemin everything;
-
-	everything.rooms = (t_room*)malloc(sizeof(t_room) * 4);
-	ft_bzero(everything.rooms, sizeof(everything.rooms));
-	everything.rooms[0].name = "start";
-	everything.rooms[0].start = TRUE;
-	everything.rooms[0].end = FALSE;
-	everything.rooms[0].visited = FALSE;
-	everything.rooms[0].index = 0;
-	everything.rooms[0].connections = (int*)malloc(sizeof(int) * 2);
-	everything.rooms[0].connections[0] = 1;
-    everything.rooms[0].connections[1] = 3;
-	everything.rooms[0].c_count = 2;
-	everything.rooms[1].name = "butts";
-	everything.rooms[1].start = FALSE;
-	everything.rooms[1].end = FALSE;
-	everything.rooms[1].visited = FALSE;
-	everything.rooms[1].index = 1;
-	everything.rooms[1].connections = (int*)malloc(sizeof(int) * 2);
-	everything.rooms[1].connections[0] = 0;
-    everything.rooms[1].connections[1] = 3;
-	everything.rooms[1].c_count = 2;
-	everything.rooms[2].name = "end";
-	everything.rooms[2].start = FALSE;
-	everything.rooms[2].end = TRUE;
-	everything.rooms[2].visited = FALSE;
-	everything.rooms[2].index = 2;
-	everything.rooms[2].connections = (int*)malloc(sizeof(int));
-	everything.rooms[2].connections[0] = 3;
-	everything.rooms[2].c_count = 1;
-	everything.rooms[3].name = "extra";
-	everything.rooms[3].start = FALSE;
-	everything.rooms[3].end = FALSE;
-	everything.rooms[3].visited = FALSE;
-	everything.rooms[3].index = 3;
-	everything.rooms[3].connections = (int*)malloc(sizeof(int) * 3);
-	everything.rooms[3].connections[0] = 0;
-    everything.rooms[3].connections[1] = 1;
-    everything.rooms[3].connections[2] = 2;
-	everything.rooms[3].c_count = 3;
-	everything.start= 0;
-	everything.end= 2;
-	everything.r_ct = 4;
-	everything.ant_count = 10;
-	if (!find_path(&everything.rooms, &everything.path, everything.start))
-		throw_error(GENERIC);
-	print_ants(&everything);
-	free(everything.path.pathfinder);
-
-	return 0;
-}*/
