f472e1a927125dde5bc7dfb9719d17c26bdee604
Brett Merrill <bmerrill@e1z2r5p10.42.us.org>
2017-06-26 19:24:40 -0700

    norm stuff and working on freeing everything

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -1,5 +1,13 @@
 #include "../includes/lem_in.h"
 
+void burn_them_all(t_lemin *lemin)
+{
+	int i;
+
+	i = 0;
+	while ()
+}
+
 int	main(void)
 {
 	t_lemin lemin;
@@ -8,4 +16,5 @@ int	main(void)
 	if (!find_path(&lemin.rooms, &lemin.path, lemin.start))
 		throw_error(GENERIC);
 	print_ants(&lemin);
+	burn_them_all(&lemin)
 }
--- srcs/solve.c
+++ srcs/solve.c
@@ -6,7 +6,7 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 19:14:08 by tpan              #+#    #+#             */
-/*   Updated: 2017/06/26 19:14:08 by tpan             ###   ########.fr       */
+/*   Updated: 2017/06/26 19:24:33 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ void		add_int_to_array(t_path *path, int index)
 	i = 0;
 	tmp = (int*)malloc(((*path).index + 1) * sizeof(int));
 	while (i < (*path).index)
-	  {
-	    tmp[i] = (*path).pathfinder[i];
-	    i++;
-	  }
+	{
+		tmp[i] = (*path).pathfinder[i];
+		i++;
+	}
 	tmp[i] = index;
 	free((*path).pathfinder);
 	(*path).pathfinder = tmp;
@@ -44,7 +44,7 @@ void		add_path(t_path *path, int index)
 	path->index++;
 }
 
-void		remove_path(t_path *path)
+int		remove_path(t_path *path)
 {
 	int *tmp;
 	int i;
@@ -53,12 +53,13 @@ void		remove_path(t_path *path)
 	tmp = (int*)malloc(((*path).index + 1) * sizeof(int));
 	while (i < (*path).index)
 	{
-	    tmp[i] = (*path).pathfinder[i];
-	    i++;
+		tmp[i] = (*path).pathfinder[i];
+		i++;
 	}
 	free((*path).pathfinder);
 	(*path).pathfinder = tmp;
 	path->index--;
+	return (0);
 }
 
 int			find_path(t_room **rooms, t_path *path, int index)
@@ -68,27 +69,21 @@ int			find_path(t_room **rooms, t_path *path, int index)
 	if (!(*rooms))
 		return 0;
 	if ((*rooms)[index].visited)
-	  return (0);
+		return (0);
 	conn_i = 0;
 	(*rooms)[index].visited = TRUE;
 	while (conn_i < (*rooms)[index].c_count &&
-			(*rooms)[(*rooms)[index].connections[conn_i]].visited == TRUE)
+		   (*rooms)[(*rooms)[index].connections[conn_i]].visited == TRUE)
 		conn_i++;
 	add_path(path, index);
 	if ((*rooms)[index].end)
-	  return (1);
+		return (1);
 	if (conn_i == (*rooms)[index].c_count)
-	{
-		remove_path(path);
-		return (0);
-	}
+		return (remove_path(path));
 	while (conn_i < (*rooms)[index].c_count &&
-			!find_path(rooms, path, (*rooms)[index].connections[conn_i]))
+		   !find_path(rooms, path, (*rooms)[index].connections[conn_i]))
 		conn_i++;
 	if (conn_i == (*rooms)[index].c_count)
-	{
-		remove_path(path);
-		return (0);
-	}
+		return (remove_path(path));
 	return (1);
 }
